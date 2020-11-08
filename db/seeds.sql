70bbeadeda03201d872c40188e90d7ad0af2f5d3
Sebastian Brear <ssbrear@ucdavis.edu>
2020-11-08 15:04:35 -0800

    Initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Protein', TRUE);
