f02b6f1abb467a10bf9c923a92b3e47c3ebd3b1f
Sebastian Brear <ssbrear@ucdavis.edu>
2020-11-08 18:21:13 -0800

    jawsdb fixing

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,18 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
-  "use_env_variable": "JAWSDB_URL",
-  "dialect": "mysql"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.local_pw,
+    database: "burgers_db",
+  });
+}
+
 
 // Makes the connection
 connection.connect((err) => {
